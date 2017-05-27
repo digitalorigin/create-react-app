e64edd5ce64f71ac53186d103a8b3ac4f4c530dd
Marcel Barros Aparici <barros.marcel92@gmail.com>
2017-05-27 20:55:13 +0200

    chore: upgrade to eslint 3

--- index.js
+++ index.js
@@ -1,40 +1,58 @@
 module.exports = {
   extends: 'airbnb',
-  env: {
-    browser: true,
-    node: true,
-    jasmine: true,
-    es6: true
-  },
   parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 6,
     sourceType: 'module',
     exmaFeatures: {
-      jsx: true
-    }
+      jsx: true,
+    },
   },
+  plugins: ['react'],
   rules: {
-    'comma-dangle': [
-      2,
-      'always-multiline'
-    ],
     indent: [
       2,
       2,
       {
-        'SwitchCase': 1
-      }
+        SwitchCase: 1,
+      },
     ],
+    'comma-dangle': [2, 'always-multiline'],
     'no-console': 0,
-    'react/prop-types': 0,
-    'max-len': 0,
+    'react/prop-types': 1,
+    'react/forbid-prop-types': 1,
+    'react/require-default-props': 1,
+    'import/no-named-as-default': 1,
     'prefer-template': 0,
-    'no-use-before-define': 0,
+    'max-len': 0,
     'newline-per-chained-call': 0,
-    'arrow-body-style': [
-      2,
-      'as-needed'
-    ]
+    'react/jsx-filename-extension': 0,
+    'no-void': 0,
+    'react/no-unescaped-entities': 0,
+    'arrow-parens': 0,
+    'no-plusplus': 0,
+    'import/first': 0,
+    'global-require': 0,
+    'react/jsx-curly-spacing': 0,
+    'react/jsx-indent': 0,
+    'import/no-mutable-exports': 0,
+    'no-prototype-builtins': 0,
+    'react/jsx-no-target-blank': 0,
+    'no-underscore': 0,
+    'no-underscore-dangle': 0,
+    'no-useless-escape': 0,
+  },
+  env: {
+    browser: true,
+    node: true,
+    jasmine: true,
+    es6: true,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['app'],
+      },
+    },
   },
 };
