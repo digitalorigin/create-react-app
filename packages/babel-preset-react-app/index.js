7f861fe9dcc231aaefb3fbeff15be11d288e33cc
Jesus Quintana <jrqb182@gmail.com>
2017-09-02 00:21:41 +0200

    feat(babel-preset): change to do standars

--- packages/babel-preset-react-app/index.js
+++ packages/babel-preset-react-app/index.js
@@ -9,8 +9,6 @@
 'use strict';
 
 const plugins = [
-  // class { handleClick = () => { } }
-  require.resolve('babel-plugin-transform-class-properties'),
   // The following two plugins use Object.assign directly, instead of Babel's
   // extends helper. Note that this assumes `Object.assign` is available.
   // { ...todo, completed: true }
@@ -21,21 +19,7 @@ const plugins = [
     },
   ],
   // Transforms JSX
-  [
-    require.resolve('babel-plugin-transform-react-jsx'),
-    {
-      useBuiltIns: true,
-    },
-  ],
-  // Polyfills the runtime needed for async/await and generators
-  [
-    require.resolve('babel-plugin-transform-runtime'),
-    {
-      helpers: false,
-      polyfill: false,
-      regenerator: true,
-    },
-  ],
+  [require.resolve('babel-plugin-transform-react-jsx')],
 ];
 
 // This is similar to how `env` works in Babel:
@@ -98,30 +82,17 @@ if (env === 'test') {
         require.resolve('babel-preset-env'),
         {
           targets: {
-            // React parses on ie 9, so we should too
-            ie: 9,
-            // We currently minify with uglify
-            // Remove after https://github.com/mishoo/UglifyJS2/issues/448
-            uglify: true,
+            ie: 10,
+            ios: 10,
+            android: '4.2',
           },
-          // Disable polyfill transforms
-          useBuiltIns: false,
-          // Do not transform modules to CJS
-          modules: false,
+          useBuiltIns: true,
         },
       ],
       // JSX, Flow
       require.resolve('babel-preset-react'),
     ],
     plugins: plugins.concat([
-      // function* () { yield 42; yield 43; }
-      [
-        require.resolve('babel-plugin-transform-regenerator'),
-        {
-          // Async functions are converted to generators by babel-preset-env
-          async: false,
-        },
-      ],
       // Adds syntax support for import()
       require.resolve('babel-plugin-syntax-dynamic-import'),
     ]),
