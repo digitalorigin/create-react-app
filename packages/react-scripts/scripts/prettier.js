6326ebada9d19168e169b8220e08a67d1d939658
Marcel Barros Aparici <barros.marcel92@gmail.com>
2017-10-20 11:36:08 +0200

    feat: add prettier react-script command

---
+++ packages/react-scripts/scripts/prettier.js
@@ -0,0 +1,39 @@
+'use strict';
+
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on('unhandledRejection', err => {
+  throw err;
+});
+
+const spawn = require('react-dev-utils/crossSpawn');
+
+const filesToPrettify = process.argv.slice(2);
+
+const prettierProcess = spawn.sync(
+  'prettier',
+  [
+    '--trailing-coma',
+    'all',
+    '--single-quote',
+    '--write',
+    '--',
+    ...filesToPrettify,
+  ],
+  {
+    stdio: 'inherit',
+  }
+);
+
+if (prettierProcess.status !== 0) {
+  console.error('prettier, Failed');
+  return;
+}
+
+const gitAddProcess = spawn.sync('git', ['add', '.'], { stdio: 'inherit' });
+
+if (gitAddProcess.status !== 0) {
+  console.error('prettier, Failed');
+  return;
+}
