d48b09d81823bd9f04bac5aa27e3ea8d41ea46ac
Sergio Diez <sdiez@pagantis.com>
2019-07-30 14:52:52 +0200

    fix(): Update eslint

--- packages/eslint-config-react-app/index.js
+++ packages/eslint-config-react-app/index.js
@@ -1,29 +1,7 @@
-/**
- * Copyright (c) 2015-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 'use strict';
 
-// Inspired by https://github.com/airbnb/javascript but less opinionated.
-
-// We use eslint-loader so even warnings are very visible.
-// This is why we only use "WARNING" level for potential errors,
-// and we don't use "ERROR" level at all.
-
-// In the future, we might create a separate list of rules for production.
-// It would probably be more strict.
-
-// The ESLint browser environment defines all browser globals as valid,
-// even though most people don't know some of them exist (e.g. `name` or `status`).
-// This is dangerous as it hides accidentally undefined variables.
-// We blacklist the globals that we deem potentially confusing.
-// To use them, explicitly reference them, e.g. `window.name` or `window.status`.
-
 module.exports = {
-  extends: ['airbnb'],
+  extends: ['airbnb', 'prettier', 'prettier/react'],
 
   parser: 'babel-eslint',
 
